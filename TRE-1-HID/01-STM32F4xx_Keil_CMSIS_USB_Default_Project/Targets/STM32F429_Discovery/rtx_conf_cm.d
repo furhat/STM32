082879ba1bfe2f9ecaab181d2269e643384ea40b
bbpa <U6015244@U6015244-TPL-A.ten.thomsonreuters.com>
2015-11-13 12:18:38 +0800

    delete some .o files

--- TRE-1-HID/01-STM32F4xx_Keil_CMSIS_USB_Default_Project/Targets/STM32F429_Discovery/rtx_conf_cm.d
+++
@@ -1,5 +0,0 @@
-.\targets\stm32f429_discovery\rtx_conf_cm.o: RTE\CMSIS\RTX_Conf_CM.c
-.\targets\stm32f429_discovery\rtx_conf_cm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.4.0\CMSIS\RTOS\RTX\INC\cmsis_os.h
-.\targets\stm32f429_discovery\rtx_conf_cm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\targets\stm32f429_discovery\rtx_conf_cm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\targets\stm32f429_discovery\rtx_conf_cm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.4.0\CMSIS\RTOS\RTX\INC\RTX_CM_lib.h
